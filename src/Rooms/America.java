706005f4b4b4dcfdc74194e20919b0708d37cc90
Dea Kurti <dkurti4965@bths.edu>
2018-12-21 11:47:12 -0500

    updated

--- src/Rooms/America.java
+++ src/Rooms/America.java
@@ -1,7 +1,58 @@
 package Rooms;
+import Game.Runner;
+import People.Person;
 
-public class America {
+
+public class America extends Room
+{
     public America(int i, int j) {
+        super (i,j);
+    }
+    private boolean inside = false;
+    private boolean alive = false;
+
+    @Override
+    public String toString() {
+        if (inside) {
+            return "[ ]";
+        } else if (alive) {
+            return "[Dead]";
+        } else {
+            return "America";
+        }
+    }
+
+    //game ending conditions//
+
+    @Override
+    public void enterRoom(Person x) {
+        inside = true;
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+
+        for (int i = 0; i < x.getInventory().length; i++) {
+            if(x.getInventory()[i] != null && x.getInventory()[i].equals("Done!")) {
+                System.out.println("Congratulations! You got into America!!");
+                Runner.gameOff();
+                break;
+            }
+            else if(i == x.getInventory().length-1){
+                System.out.println("Oh no!!");
+            }
+        }
+
 
     }
+
+    @Override
+    public void leaveRoom(Person x) {
+        super.leaveRoom(x);
+        occupant = null;
+        inside = false;
+    }
 }
+    }
+}
+
+
